134914a79692c28b87dcffb041116b8ceb406246
isuraem <erandaim@gmail.com>
2024-05-16 19:50:04 +0530

    change_db_configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
-
+import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true
-    }),
+    DatabaseModule,
     UserModule,
     CompanyModule,
-  ]
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
